1196bf4667e0e75ace8f75727e63de3768e99303
jmeisele <jmeisele@yahoo.com>
2020-05-25 06:42:12 -0500

    :rocket:

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
 def index():
-    return "Hello from Flask!"
+    return "Hello from Flask"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
