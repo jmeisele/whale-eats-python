413210993dab21c30a0a2b4fb05b80198aebd7a1
jmeisele <jmeisele@yahoo.com>
2020-06-07 05:21:36 -0500

    :whale: updated dockerfiles

--- app2/app.py
+++ app2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
 def index():
-    return "Hello again from Flask"
+    return "Hello from Flask version 2!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3001, debug=True)
